75b6538638462b8c6325fe24b6bc74ac8ce404ec
victorfu14 <victorfu05@hotmail.com>
2019-10-12 03:56:43 -0400

    moved files

---
+++ static/js/hello.js
@@ -0,0 +1,9 @@
+alert("hello!");
+console.log("hello");
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
